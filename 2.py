b54e65e59981ab4253916deca1cdc049d6d2a4ba
Wt666 <20431546@life.hkbu.edu.hk>
2021-01-21 15:18:48 +0800

    3rd commit

---
+++ 2.py
@@ -0,0 +1,9 @@
+# Write a function that prints all factors of the given parameter x
+x=52633
+def print_factor(x):
+  for i in range(1,x+1):
+      if x % i == 0:
+          print(i)
+  print(x)
+
+print_factor(x)
