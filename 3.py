546c81e9bf25916208bcd42a64ff22dbc5601482
Wt666 <20431546@life.hkbu.edu.hk>
2021-01-22 10:38:48 +0800

    change
    change

--- 3.py
+++ 3.py
@@ -1,8 +1,8 @@
 # Write a program that be able to find all factors of the numbers in the list l
 l = [52633, 8137, 1024, 999]
 
-def print_factor(list_factor):
-    for j in list_factor:
+def print_factor(l_factor):
+    for j in l_factor:
         print("")
         for i in range(1,1+j):
             if j % i == 0:
